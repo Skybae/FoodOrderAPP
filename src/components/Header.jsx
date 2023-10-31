928d3cb206ad9b722a245a668a6e85a84bb5147e
Sheetal Yadav <110325987+Skybae@users.noreply.github.com>
2023-10-31 17:24:25 +0530

    Cart Context
    Items added in cart and removed and data shown in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
-import logoImg from "../assets/logo.jpg"
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
 
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+    const totalCardItems = cartCtx.items.reduce((totalNumberOfItems, item)=>{
+        return totalNumberOfItems + item.quantity;
+    }, 0);
     return (
         <header id="main-header">
             <div id="title">
@@ -8,7 +15,7 @@ export default function Header() {
                 <h1>Food Order APP</h1>
             </div>
             <nav>
-                <button>Cart(0)</button>
+                <Button textOnly>Cart({totalCardItems})</Button>
             </nav>
         </header>
     )
